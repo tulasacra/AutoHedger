b6589e1135cc9bc2a3a3ddc583b01569987c4c94
tulasacra <tulasacra@gmail.com>
2024-10-02 14:20:21 +0200

    removed status column

--- AutoHedger/Program.cs
+++ AutoHedger/Program.cs
@@ -27,10 +27,10 @@ namespace AutoHedger
             const string delimiter = "--------------------------------------------------------------------------------";
             Console.Clear();
             Console.WriteLine($"Checking at: {DateTime.Now}");
+            Console.WriteLine($"Minimum desired APY: {AppSettings.MinimumApy} %");
             Console.WriteLine(delimiter);
 
             const string counterLeverage = "5"; //only check 20% hedge
-            var premiumData = await Premiums.GetPremiums(currencyOracleKey, counterLeverage, 0);
             decimal latestPrice = await OraclesCash.OraclesCash.GetLatestPrice(currencyOracleKey);
             
             
@@ -58,9 +58,12 @@ namespace AutoHedger
 
 
             Console.WriteLine(delimiter);
-            DisplayPremiumsData(premiumData, latestPrice);
+            var premiumData = (await Premiums.GetPremiums(currencyOracleKey, counterLeverage, 0))
+                .Where(x => x.Apy >= AppSettings.MinimumApy)
+                .ToList();
+            DisplayPremiumsData(premiumData);
 
-            if (walletBalanceBch.HasValue)
+            if (walletBalanceBch.HasValue && premiumData.Any())
             {
                 var bestContractParameters = GetBestContractParameters(premiumData, walletBalanceBch.Value);
                 if (bestContractParameters.HasValue)
@@ -74,30 +77,22 @@ namespace AutoHedger
             Console.WriteLine("Press [Enter] to exit the program.");
         }
 
-        private static void DisplayPremiumsData(List<PremiumDataItem> premiumData, decimal priceDelta)
+        private static void DisplayPremiumsData(List<PremiumDataItem> premiumData)
         {
-            Console.WriteLine("| Amount (BCH) | Duration (days) | Premium (%) | APY (%) | Status |");
-            Console.WriteLine("|--------------|-----------------|-------------|---------|--------|");
+            Console.WriteLine("| Amount (BCH) | Duration (days) | Premium (%) | APY (%) |");
+            Console.WriteLine("|--------------|-----------------|-------------|---------|");
 
             foreach (var item in premiumData)
             {
-                string status = item.Apy >= AppSettings.MinimumApy ? "OK" : "";
-
-                Console.WriteLine($"| {item.Amount,12} | {item.Duration,15} | {item.PremiumInfo.Total,11:F2} | {item.Apy,7:F2} | {status,-6} |");
+                Console.WriteLine($"| {item.Amount,12} | {item.Duration,15} | {item.PremiumInfo.Total,11:F2} | {item.Apy,7:F2} |");
             }
         }
 
         private static (decimal amount, double duration)? GetBestContractParameters(List<PremiumDataItem> premiumData, decimal walletBalanceBch)
         {
             var candidates = premiumData
-                .Where(x => x.Apy >= AppSettings.MinimumApy)
                 .GroupBy(x=>x.Amount)
                 .ToList();
-
-            if (!candidates.Any())
-            {
-                return null;
-            }
 
             var candidatesThatFitWholeBalance = candidates.Where(x => x.Key >= walletBalanceBch).ToList();
             if (candidatesThatFitWholeBalance.Any())
