4f059147d597a6e473d783215402b704701f182c
tulasacra <tulasacra@gmail.com>
2024-11-29 20:58:36 +0100

    optimized api call - dont ask for balances for accounts that have no wallet address filled

--- AutoHedger/TermedDepositAccount.cs
+++ AutoHedger/TermedDepositAccount.cs
@@ -23,7 +23,7 @@ public class TermedDepositAccount
 
     public static async Task<TermedDepositAccount[]> Get(List<WalletConfig> wallets)
     {
-        List<Task<TermedDepositAccount>> tasks = new (wallets.Count);
+        List<Task<TermedDepositAccount>> tasks = new(wallets.Count);
         foreach (var wallet in wallets)
         {
             var oracleKey = OracleKeys.Keys[wallet.Currency];
@@ -33,26 +33,29 @@ public class TermedDepositAccount
                 return new TermedDepositAccount(wallet, metadata, oracleKey);
             }));
         }
-        
+
         return await Task.WhenAll(tasks);
     }
 
     public static async Task UpdateLatestPrices(TermedDepositAccount[] accounts)
     {
-        decimal[] latestPrice = await OraclesCashService.GetLatestPrice(accounts.Select(x=> (x.OracleKey, x.OracleMetadata)).ToArray());
+        decimal[] latestPrice = await OraclesCashService.GetLatestPrice(accounts.Select(x => (x.OracleKey, x.OracleMetadata)).ToArray());
         for (int i = 0; i < accounts.Length; i++)
         {
             accounts[i].LatestPrice = latestPrice[i];
             accounts[i].WalletBalance = accounts[i].WalletBalanceBch * accounts[i].LatestPrice;
         }
     }
-    
+
     public static void UpdateWalletBalances(TermedDepositAccount[] accounts)
     {
         var bchClient = new BitcoinCashClient();
-        var results = bchClient.GetWalletBalances(accounts.Select(x => x.Wallet.Address).ToList());
+        var results = bchClient.GetWalletBalances(accounts
+            .Where(x => x.Wallet.HasAddress)
+            .Select(x => x.Wallet.Address)
+            .ToList());
 
-        foreach (var account in accounts.Where(x=>x.Wallet.HasAddress))
+        foreach (var account in accounts.Where(x => x.Wallet.HasAddress))
         {
             account.WalletBalanceBch = results.FirstOrDefault(x => x.Key == account.Wallet.Address).Value / 100_000_000m;
             account.WalletBalance = account.WalletBalanceBch.Value * account.LatestPrice;
